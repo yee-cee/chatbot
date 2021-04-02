aac59ec673d0f43d1d5404f6afc73221101d4f5c
caramelyc <52923150+caramelyc@users.noreply.github.com>
2021-04-02 08:08:44 +0800

    Add files via upload

---
+++ SwiftUIChat/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  SwiftUIChat
+//
+//  Created by Mason on 20/3/2021.
+//
+
+import UIKit
+import ApiAI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let configuration = AIDefaultConfiguration()
+        configuration.clientAccessToken = "f6f6bb91bf27ea31248e14fa046314ee1179eee9"
+        
+        let apiai = ApiAI.shared()
+        apiai?.configuration = configuration
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
